82d6e4a8f035fbbaa9099f3b8f7695942a37d4cf
Tyler Laptop <FreezeKickerz@icloud.com>
2021-09-30 21:45:56 -0400

    Test

--- Test1.py
+++ Test1.py
@@ -1,16 +1,18 @@
 x = input("Please enter a character: ")
 if type(x)==int:
-    if x<=3:
+    if int(x)>=0 & int(x)<=3:
         print("small")
-    elif x>=4 & x <=6:
+    elif int(x)>=4 & int(x)<=6:
         print("medium")
-    elif x>=7 & x<=9:
+    elif int(x)>=7 & int(x)<=9:
         print("large")
-
-if ord(x)>= 97 & ord(x)<=122:
-    print((ord(x)-96)*x)
-elif ord(x)>=65 & ord(x)<=90:
-    newInt = ord(x) + 32
-    print(chr(70))
+elif type(x) !=int:
+    if (ord(x)>=65)&(ord(x)<=90):
+        newInt = ord(x)+32
+        print(chr(newInt))
+    elif (ord(x)>= 97)&(ord(x)<=122):
+        print((ord(x)-96)*x)
+    else:
+        print("Invalid")
 else:
     print("Invalid")
