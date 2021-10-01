bfa5b0ac229db0db558573349755ad06c0f47e8e
Tyler Laptop <FreezeKickerz@icloud.com>
2021-10-01 13:23:54 -0400

    Test

--- Binary.py
+++ Binary.py
@@ -5,17 +5,14 @@ digitThree = (userInput - (digitOne*10000 + digitTwo*1000))//100
 digitFour = (userInput - (digitOne*10000 + digitTwo*1000 + digitThree*100))//10
 digitFive = (userInput - (digitOne*10000 + digitTwo*1000 + digitThree*100 + digitFour*10))//1
 
-if digitOne <= 1 & digitTwo <= 1 & digitThree <= 1 & digitFour <= 1 & digitFive <= 1:
-    if digitOne <= 1 & digitTwo <= 1 & digitThree <= 1 & digitFour <= 1 & digitFive <= 1:
+if userInput//100000 > 0:
+    print("Over 5 digits")
+elif digitOne <= 1 and digitTwo <= 1 and digitThree <= 1 and digitFour <= 1 and digitFive <= 1:
         digitOne = (2**4)*digitOne
         digitTwo = (2**3)*digitTwo
         digitThree = (2**2)*digitThree
         digitFour = (2**1)*digitFour
         digitFive = (2**0)*digitFive
-        print(digitOne + digitTwo + digitThree + digitFour + digitFive)
-    else: 
-        print("Over 5 digits") 
-elif userInput//100000 > 0:
-    print("Over 5 digits")
+        print(digitOne + digitTwo + digitThree + digitFour + digitFive) 
 else: 
     print("Not a valid binary number")
