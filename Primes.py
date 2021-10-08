afe393aeb2ad2b6b9097c7593f5ac5ae2e968259
Tyler Desktop <FreezeKickerz@icloud.com>
2021-10-08 01:31:10 -0400

    Save

--- Primes.py
+++ Primes.py
@@ -1,1 +1,31 @@
-userInput = input("Please Enter n: ")
+userInput = int(input("Please Enter n: "))
+
+higherPrimes = []
+lowerPrimes =[]
+counter = userInput
+
+i = 0
+while i < 5:
+# Find 5 Lower Primes
+    counter+=1
+    for j in range(2, counter):
+        if counter % j == 0:
+            break
+    else: 
+        higherPrimes.append(counter)
+        i+=1
+# End Code for Higher Primes
+counter=userInput
+p = 0
+while p < 5:
+    counter-=1
+    for j in range(2,counter):
+        if counter % j == 0:
+            break
+    else:
+        lowerPrimes.append(counter)
+        p+=1
+    if counter <= 2:
+        break
+print("Larger primes: ", higherPrimes[0],higherPrimes[1],higherPrimes[2],higherPrimes[3],higherPrimes[4])
+print("Smaller primes: ", lowerPrimes[0],lowerPrimes[1],lowerPrimes[2],lowerPrimes[3],lowerPrimes[4])
