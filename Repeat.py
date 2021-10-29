611bb8acbd61ee76aa30bec8f1fa1076cda85969
Tyler Laptop <FreezeKickerz@icloud.com>
2021-10-29 11:11:41 -0400

    Save

--- Repeat.py
+++ Repeat.py
@@ -1,10 +1,15 @@
 input = input("Please enter the string: ")
-letters = []
+counter = 0
+maxCounter = 0
+maxCharacter = ""
+previousCharacter = ""
 for x in input:
-    input[x] 
-
-
-
-
-
-#print("The longest sequence is b repeated ", x, "times")
+    if previousCharacter == x:
+        counter += 1
+    else:
+        counter = 1
+    if counter > maxCounter:
+        maxCounter = counter
+        maxCharacter = x
+    previousCharacter = x
+print("The longest sequence is", maxCharacter, "repeated", maxCounter, "times")
